1a82c695b6cf5335ca8b7d2c7b8b84bf5c32f547
Arcloan <95delchris@gmail.com>
2025-01-14 23:01:38 +0100

    finished the header with the nav. Implemented the hamburger menu with popover attribute

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Inter": ["Inter", ]
+      },
+      colors: {
+        "softOrange": "hsl(35,77%,62%)",
+        "softRed": "hsl(5, 85%, 63%)",
+        "offWhite": "hsl(36,100%,99%)",
+        "grayshBlue": "hsl(233,8%,79%)",
+        "darkGrayshBlue": "hsl(236,13%,42%)",
+        "veryDarkBlue": "hsl(240,100%,5%)",
+      }
+    },
   },
   plugins: [],
 }
